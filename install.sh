3492e43e40c6c135a9fbc51ecef4bdd02c46fd00
Jerome Lemuel Basa <jerome@jerome.hpls.local>
2012-09-10 10:40:03 +0800

    more cleaning-up

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo "initializing submodules"
-git submodule init
-git submodule update
+#echo "initializing submodules"
+#git submodule init
+#git submodule update
 
 echo "Back-up files to ~/.dotfiles_old"
 mkdir ~/.dotfiles_old
@@ -16,14 +16,13 @@ mv ~/.gitignore ~/.dotfiles_old
 mv ~/.tmux.conf ~/.dotfiles_old
 
 echo "Symlinking files"
-ln -s ~/Github/dotfiles/vimrc ~/.vimrc
+#ln -s ~/Github/dotfiles/vimrc ~/.vimrc
+#ln -s ~/Github/dotfiles/vim ~/.vim
+#ln -s ~/Github/dotfiles/gvimrc ~/.gvimrc
 ln -s ~/Github/dotfiles/zshrc ~/.zshrc
-ln -s ~/Github/dotfiles/vim ~/.vim
-ln -s ~/Github/dotfiles/gvimrc ~/.gvimrc
 ln -s ~/Github/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/Github/dotfiles/gitignore ~/.gitignore
 ln -s ~/Github/dotfiles/tmux ~/.tmux.conf
-ln -s ~/Github/dotfiles/githelpers ~/.githelpers
 
 #echo "Updating submodules"
 #git submodule foreach git pull origin master --recurse-submodules
