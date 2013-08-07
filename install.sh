1db3d323c664fcce3fd4b9575c27bf4331cc5db2
Jerome Basa <jerome@basa.ph>
2013-08-07 07:20:17 -0700

    changed the directory

--- install.sh
+++ install.sh
@@ -19,18 +19,18 @@ mv ~/.todo.cfg ~/.vimrc ~/.dotfiles_old
 mv ~/.zshrc ~/.dotfiles_old
 
 echo "Symlinking files"
-ln -s ~/Github/dotfiles/editrc ~/.editrc
-ln -s ~/Github/dotfiles/gemrc ~/.gemrc
-ln -s ~/Github/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/Github/dotfiles/gitignore ~/.gitignore
-ln -s ~/Github/dotfiles/gvimrc ~/.gvimrc
-ln -s ~/Github/dotfiles/inputrc ~/.inputrc
-ln -s ~/Github/dotfiles/pentadactylrc ~/.pentadactylrc
-ln -s ~/Github/dotfiles/vim ~/.vim
-ln -s ~/Github/dotfiles/vimrc ~/.vimrc
-ln -s ~/Github/dotfiles/tmux ~/.tmux.conf
-ln -s ~/Github/dotfiles/todo.cfg ~/.todo.cfg
-ln -s ~/Github/dotfiles/zshrc ~/.zshrc
+ln -s ~/src/dotfiles/editrc ~/.editrc
+ln -s ~/src/dotfiles/gemrc ~/.gemrc
+ln -s ~/src/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/src/dotfiles/gitignore ~/.gitignore
+ln -s ~/src/dotfiles/gvimrc ~/.gvimrc
+ln -s ~/src/dotfiles/inputrc ~/.inputrc
+ln -s ~/src/dotfiles/pentadactylrc ~/.pentadactylrc
+ln -s ~/src/dotfiles/vim ~/.vim
+ln -s ~/src/dotfiles/vimrc ~/.vimrc
+ln -s ~/src/dotfiles/tmux ~/.tmux.conf
+ln -s ~/src/dotfiles/todo.cfg ~/.todo.cfg
+ln -s ~/src/dotfiles/zshrc ~/.zshrc
 
 #echo "Updating submodules"
 #git submodule foreach git pull origin master --recurse-submodules
