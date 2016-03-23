922f39ec78780f3f9e224c41eb95e2da3cafa9e7
Jerome Basa <jerome@teambanjo.com>
2016-03-23 06:35:21 -0700

    More scripts

---
+++ scripts/sync_music.sh
@@ -0,0 +1,4 @@
+find ~/Music/iTunes/iTunes\ Media -iname \*.mp3  -exec cp {} /Volumes/UNTITLED/ \;
+find ~/Music/iTunes/iTunes\ Media -iname \*.mpa  -exec cp {} /Volumes/UNTITLED/ \;
+find ~/Music/iTunes/iTunes\ Media -iname \*.m4a  -exec cp {} /Volumes/UNTITLED/ \;
+find ~/Music/iTunes/iTunes\ Media -iname \*.m4p  -exec cp {} /Volumes/UNTITLED/ \;
