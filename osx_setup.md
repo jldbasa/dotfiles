0325ac97c86a03b7ea7a1b579c7900b18d156fb3
Jerome Basa <jeromebasa@gmail.com>
2021-08-04 20:04:48 -0700

    init

--- osx_setup.md
+++
@@ -1,2 +0,0 @@
-### Applications
-- [mac2imgur](https://github.com/mileswd/mac2imgur) - auto upload screenshots to imgur
--- xubuntu.sh
+++
@@ -1,5 +0,0 @@
-# remap capslock key to control
-/usr/bin/setxkbmap -option "ctrl:nocaps"
-
-# load ICC profile
-xcalib -d :0 -s 0 ~/.local/share/icc/hpprobook6555b.icc
